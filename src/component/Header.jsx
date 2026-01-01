cc6e2e99ba09819aca879f65475c153b742cc13f
Nahidul-Islam <mdnahid784055@gmail.com>
2026-01-01 11:24:25 +0600

    added coustom theme & date-fns intregration

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
   return (
     <div>
       <div className="flex justify-center items-center mt-10 flex-col">
-        <img src={logo} className='w-87.5 mb-5' alt="title logo" />
-        <p>Journalism Without Fear or Favour</p>
+        <img src={logo} className="w-87.5 mb-5" alt="title logo" />
+        <p className="text-accent">Journalism Without Fear or Favour</p>
+        <p className='text-accent mt-2'>{format(new Date(), 'EEEE, MMMM MM, yyyy')}</p>
       </div>
     </div>
   );
