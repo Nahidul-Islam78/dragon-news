7a41795ceb38ee688892166659045758fc686267
Nahidul-Islam <mdnahid784055@gmail.com>
2026-01-08 14:31:12 +0600

    added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
-  return (
-    <div>
-      <p>home</p>
-    </div>
-  );
+  return <Navigate to='/category/0'></Navigate>
+    
 };
 
 export default Home;
